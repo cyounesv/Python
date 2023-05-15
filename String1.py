16f14120b233d198e01a804d65770defbd628c28
chloeyounes <55399912+cyounesv@users.noreply.github.com>
2023-05-15 10:53:24 +0200

    Update String1.py

--- String1.py
+++ String1.py
@@ -19,51 +19,6 @@ x = 'Chloe'
 both_ends(x)
 
 
-def fix_start(s):
-    first_char = s[0]
-    rest = s[1:]
-    tmp = rest.replace(first_char, '*')
-    return first_char + tmp
-
-
-def mix_up(a, b):
-    a_swapped = b[:2] + a[2:]
-    b_swapped = a[:2] + b[2:]
-    return a_swapped + ' ' + b_swapped
-
-
-def test(got, expected):
-    if got == expected:
-        prefix = ' OK '
-    else:
-        prefix = '  X '
-    print('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
-
-
-def main():
-
-    # Each line calls donuts, compares its result to the expected for that call.
-    test(donuts(4), 'Number of donuts: 4')
-    test(donuts(9), 'Number of donuts: 9')
-    test(donuts(10), 'Number of donuts: many')
-    test(donuts(99), 'Number of donuts: many')
-
-    test(both_ends('spring'), 'spng')
-    test(both_ends('Hello'), 'Helo')
-    test(both_ends('a'), '')
-    test(both_ends('xyz'), 'xyyz')
-
-    test(fix_start('babble'), 'ba**le')
-    test(fix_start('aardvark'), 'a*rdv*rk')
-    test(fix_start('google'), 'goo*le')
-    test(fix_start('donut'), 'donut')
-
-    test(mix_up('mix', 'pod'), 'pox mid')
-    test(mix_up('dog', 'dinner'), 'dig donner')
-    test(mix_up('gnash', 'sport'), 'spash gnort')
-    test(mix_up('pezzy', 'firm'), 'fizzy perm')
-
-
 if __name__ == '__main__':
     main()
 def test():
